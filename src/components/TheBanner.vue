76e8e114ac2225866fbb631ee75a14c85a6b05de
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2023-07-26 10:17:22 +0800

    Initial commit

---
+++ src/components/TheBanner.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <p class="title has-text-light">Barangay Directory</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  background: #022735;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheForm from '@/components/TheForm.vue'
+import TheBanner from '@/components/TheBanner.vue'
+</script>
+
+<template>
+  <main>
+    <TheBanner />
+    <TheForm />
+  </main>
+</template>
